5521b278a5ae794d34bd282e20eb15d439232164
rania3103 <raniaabassi33@gmail.com>
2023-09-20 12:40:10 +0100

    add library

--- sort.h
+++ sort.h
@@ -1,7 +1,8 @@
 #ifndef SORT
 #define SORT
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,6 +16,8 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
