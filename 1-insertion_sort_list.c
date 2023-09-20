5657037b3e526585ad4ce4bde35e4f063d27d712
Mariam Charrada <charradamariam@yahoo.com>
2023-09-20 13:05:04 -0700

    first push

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list: list of integers to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *slot, *prev, *current;
+
+	if ((*list)->next)
+	{
+		current = (*list)->next;
+		while (current)
+		{
+			slot = current;
+			prev = current->prev;
+			while (slot->n < prev->n)
+			{
+				prev->next = slot->next;
+				slot->prev = prev->prev;
+				prev->prev = slot;
+				slot->next = prev;
+				print_list(*list);
+			}
+			current = current->next;
+		}
+	}
+	return;
+}
