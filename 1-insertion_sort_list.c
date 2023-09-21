f72d91f3d03638e2edcc964e3396ef56540d7d2d
Mariam Charrada <charradamariam@yahoo.com>
2023-09-21 06:53:31 -0700

    change order

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,29 +1,30 @@
 #include "sort.h"
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * insertion_sort_list - sorts a doubly linked
+ * list of integers in ascending order
  * @list: list of integers to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *slot, *prev, *current;
 
-	if ((*list)->next && list)
+	if (!(*list)->next || !list || !(*list))
+		return;
+
+	current = (*list)->next;		
+	while (current)
 	{
-		current = (*list)->next;
-		while (current)
+		slot = current;
+		prev = current->prev;
+		while (slot->n < prev->n)
 		{
-			slot = current;
-			prev = current->prev;
-			while (slot->n < prev->n)
-			{
-				prev->next = slot->next;
-				slot->prev = prev->prev;
-				prev->prev = slot;
-				slot->next = prev;
-				print_list(*list);
-			}
-			current = current->next;
+			slot->next = prev;
+			slot->prev = prev->prev;
+			prev->prev = slot;
+			prev->next = slot->next;
+			print_list(*list);
 		}
+		current = current->next;
 	}
 	return;
 }
